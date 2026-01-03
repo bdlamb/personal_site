60430571c97898c51019d79e75ead5b91ec5521a
brain lamb <bdlamb3220@yahoo.com>
2026-01-03 14:23:34 -0800

    edits to improve dark mode, moved css stuff in html to styles.

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 function darkModeChange(e){
     document.body.classList.toggle('dark-mode');
-    /*console.log(e);
-    if(e.currentTarget.checked){
-        document.body.classList.toggle('dark-mode');
-    }
-    else{
-        document.body.classList.toggle('dark-mode');
-    }*/
+}
+
+function darkModeChangeCompact(){
+    $("#check").trigger("click");
 }
 
 $("#check").on("click",darkModeChange);
 
 changePageContent("home");
+checkColoring();
+
+function checkColoring(){
+    var darkThemPefered=window.matchMedia('(prefers-color-scheme: dark)');
+    if(darkThemPefered.matches){
+        $("#check").trigger("click");
+    }
+}
 
 async function changePageContent(swapPage){
     var file= await fetch(swapPage.toLowerCase()+".html");
     var html=await file.text();
-    console.log(html);
+    //console.log(html);
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
     const element=doc.querySelector("div");
